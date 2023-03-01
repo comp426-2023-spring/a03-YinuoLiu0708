2cbd109b25d6dd6a2fa9bddc554f540cbb18b703
Yinuo Liu <77820812+YinuoLiu0708@users.noreply.github.com>
2023-03-01 17:13:37 -0500

    change to console.error

--- bin/rpsls-cli.js
+++ bin/rpsls-cli.js
@@ -19,7 +19,7 @@ try{
     console.log(JSON.stringify(rpsls(argument._[0])));
 } catch (error){
     if (error instanceof RangeError){
-        console.log('Your input is out of range');
+        console.error('Your input is out of range');
         rules();
         process.exit();
     }
